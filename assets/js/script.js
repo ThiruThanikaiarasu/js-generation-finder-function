f0134a901a7c7bdf8c2590b3ad12c69cd90dc716
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-03-21 20:50:56 +0530

    Add. FindGeneration Function added.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,49 @@
-// this is script 
+function FindGeneration(code,gender)
+{
+    if((gender == 'm' || gender == 'M') && code<0)
+    {
+        relation = " father"
+    }
+    else if((gender == 'f' || gender == 'f') && code<0)
+    {
+        relation = " mother"
+    }
+    else if((gender == 'm' || gender == 'M') && code>0)
+    {
+        relation = " son"
+    }
+    else if((gender == 'f' || gender == 'f') && code>0)
+    {
+        relation = " daughter"
+    }
+
+    switch(code)
+    {
+        case -3:
+            console.log("great grand"+relation)
+            break
+        case -2:
+            console.log("grand"+relation)
+            break
+        case -1:
+            console.log(relation)
+            break
+        case 0:
+            console.log("me")
+            break
+        case 1:
+            console.log(relation)
+            break
+        case 2:
+            console.log("grand"+relation)
+            break
+        case 3:
+            console.log("great grand"+relation)
+            break
+    }
+}
+
+var code = 0
+var gender = 'f'
+var relation
+FindGeneration(code,gender)
